fa2fbe668b1ae3c138f6cde1306cd8c4e52488cc
Chaimi Lee <95277674+chaimilee@users.noreply.github.com>
2022-12-21 14:37:59 +0900

    Update chaimilee.py

--- chaimilee.py
+++ chaimilee.py
@@ -23,7 +23,8 @@ def main():
         st.write("""I am a data science and mathematics enthusiast currently pursuing a Masters degree in Applied Data Science at the University of Southern California. I received my Bachelors degree in Mathematics with a minor in Physics from Santa Clara University. My technical skills include proficiency in Python and I am knowledgeable in statistical/machine learning.\n
 In my previous internship at Intel Corporation, I developed and implemented automated solutions to improve workflow efficiency as a software technical consulting engineer. \n
 I am seeking opportunities to utilize my skills and experience to make a positive impact in the data science field. \n
-Feel free to connect with me to learn more about my background and how I can contribute to your organization!
+Feel free to connect with me to learn more about my background and how I can contribute to your organization!\n
+During my free time, I like to go to CorePower yoga, try new restaurants, read about horoscopes, go on hikes, or go for a drive (I finally got my license this year 😎)
             """)
 
     st.markdown("[Feel free to connect with me on Linkedin](https://www.linkedin.com/in/chaimi-lee/)")
